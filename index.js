6f5e078fc245018e126d4b1183a654917e3fa367
seeya <s10094128@alumni.np.edu.sg>
2020-09-06 05:41:42 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const QRCode = require('qrcode');
+
+http.createServer((req, res) => {
+  const code = decodeURIComponent(req.url.split("/?text=")[1]);
+  console.log(code);
+  QRCode.toFileStream(res, code, {
+    margin: 1,
+    width: 200
+  });
+
+}).listen(8080);
