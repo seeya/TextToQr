26af0914197e9739464316d5b06b5212ba099ade
seeya <s10094128@alumni.np.edu.sg>
2020-09-06 05:57:31 +0000

    Added some error handling

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const http = require("http");
 const QRCode = require('qrcode');
 
 http.createServer((req, res) => {
-  const code = decodeURIComponent(req.url.split("/?text=")[1]);
-  console.log(code);
-  QRCode.toFileStream(res, code, {
-    margin: 1,
-    width: 200
-  });
-
+  try {
+    const code = decodeURIComponent(req.url.split("/?text=")[1]);
+    console.log(code);
+    if(!code || code == "undefined")
+      return res.end("Error");
+    
+    QRCode.toFileStream(res, code, {
+      margin: 1,
+      width: 200
+    });
+  } catch(err) {
+    res.end("Error");
+  }
 }).listen(process.env.PORT ? process.env.PORT : 8080);
